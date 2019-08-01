d0a9e420094bc9e8a4e5d81b8a4c7c02b8f541c0
Mary Skr <maryskr93@gmail.com>
2019-08-01 20:20:39 +0300

    render uploaded products table if errors has been occured

--- app/controllers/spree/admin/products/imports_controller.rb
+++ app/controllers/spree/admin/products/imports_controller.rb
@@ -2,15 +2,20 @@ module Spree
   module Admin
     module Products
       class ImportsController < Spree::Admin::BaseController
+        include ::Spree::Admin::NavigationHelper
 
         def new; end
 
         def create
           result = ::Spree::Admin::Products::Imports::CreateService.call(import_params[:file])
           @errors = result.error
-          @collection = ::Spree::Product.where(id: result.value)
-          @search = @collection.ransack(params[:q]&.reject { |k, _v| k.to_s == 'deleted_at_null' })
-          render :new
+          if @errors.any?
+            @collection = ::Spree::Product.where(id: result.value)
+            render :new
+          else
+            flash[:success] = t('.success')
+            redirect_to admin_products_path
+          end
         end
 
         private
