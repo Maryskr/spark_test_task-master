2cec07a3745805bc2960e0434a6e69da93685425
Mary Skr <maryskr93@gmail.com>
2019-08-01 21:04:21 +0300

    create stock item with count on hand

--- app/services/spree/admin/products/imports/create_service.rb
+++ app/services/spree/admin/products/imports/create_service.rb
@@ -19,6 +19,7 @@ module Spree
               product.price = row['price']
               if product.valid?
                 product.save
+                stock_item = build_stock_item(product, row)
                 products << product.id
               else
                 errors << { index: $., message: product.errors&.full_messages&.join(', ') }
@@ -37,6 +38,14 @@ module Spree
               available_on: row['availability_date']&.to_date,
               tax_category_id: ::Spree::TaxCategory.find_or_create_by(name: row['category'])&.id,
               shipping_category_id: ::Spree::ShippingCategory.first.id
+            )
+          end
+
+          def build_stock_item(product, row)
+            product.stock_items.create(
+              variant_id: product.master.id,
+              count_on_hand: row['stock_total'].to_i,
+              stock_location_id: ::Spree::StockLocation.active.first.id
             )
           end
 
