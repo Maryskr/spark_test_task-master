4fe3ce9f1aaebde1a2f05334b9d138021094f0c5
Mary Skr <maryskr93@gmail.com>
2019-08-02 10:38:44 +0300

    add SmarterCSV; fix price parsing

--- app/services/spree/admin/products/imports/create_service.rb
+++ app/services/spree/admin/products/imports/create_service.rb
@@ -13,10 +13,11 @@ module Spree
 
           def call
             options = { skip_blanks: true, headers: true, col_sep: ';' }
-            CSV.foreach(file.path, options) do |row|
-              next unless row['name'].present? || row['price'].present?
+            ::SmarterCSV.process(file.path, options) do |chunk|
+              row = chunk.first
+              next unless row[:name].present? || row[:price].present?
               product = build_from_row(row)
-              product.price = row['price']
+              product.price = price_from_string row[:price]
               if product.valid?
                 product.save
                 stock_item = build_stock_item(product, row)
@@ -32,11 +33,11 @@ module Spree
 
           def build_from_row(row)
             ::Spree::Product.new(
-              name: row['name'],
-              description: row['description'],
-              slug: row['slug'],
-              available_on: row['availability_date']&.to_date,
-              tax_category_id: ::Spree::TaxCategory.find_or_create_by(name: row['category'])&.id,
+              name: row[:name],
+              description: row[:description],
+              slug: row[:slug],
+              available_on: row[:availability_date]&.to_date,
+              tax_category_id: ::Spree::TaxCategory.find_or_create_by(name: row[:category])&.id,
               shipping_category_id: ::Spree::ShippingCategory.first.id
             )
           end
@@ -44,9 +45,13 @@ module Spree
           def build_stock_item(product, row)
             product.stock_items.create(
               variant_id: product.master.id,
-              count_on_hand: row['stock_total'].to_i,
+              count_on_hand: row[:stock_total].to_i,
               stock_location_id: ::Spree::StockLocation.active.first.id
             )
+          end
+
+          def price_from_string(price)
+            price.gsub(/[^\d^\.]/, '').to_f / 100
           end
 
           attr_accessor :file, :products, :errors
